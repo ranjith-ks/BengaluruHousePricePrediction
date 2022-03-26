be467381b7aeab309de8e441cddff3b75fc92aff
ranjith.ks <ranjith.ks@outlook.com>
2022-03-26 16:27:56 +0530

    feat: Deployment Changes

--- Client/app.js
+++ Client/app.js
@@ -26,15 +26,15 @@ function onClickedEstimatePrice() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    // var url = "http://127.0.0.1:5000/predict_home_price";
+    var url = "/api/predict_home_price";
 
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
         bhk: bhk,
         bath: bathrooms,
         location: location.value
-    }, function(data, status) {
+    }, function (data, status) {
         console.log(data.estimated_price);
         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
         console.log(status);
@@ -43,9 +43,9 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
     console.log("document loaded");
-    // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-    var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-    $.get(url, function(data, status) {
+    // var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "/api/get_location_names";
+    $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
         if (data) {
             var locations = data.locations;
